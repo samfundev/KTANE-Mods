ee0a04cf736f180afbf5ef6cbd2ae0c0b3ad4797
samfun123 <samdinner@hotmail.com>
2017-09-10 17:28:45 -0400

    Added Switches Support.

---
+++ TwitchPlays/Assets/Scripts/ComponentSolvers/Modded/Misc/SwitchesComponentSolver.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchesComponentSolver : ComponentSolver
+{
+	public SwitchesComponentSolver(BombCommander bombCommander, MonoBehaviour bombComponent, IRCConnection ircConnection, CoroutineCanceller canceller) :
+		base(bombCommander, bombComponent, ircConnection, canceller)
+	{
+		_component = bombComponent.GetComponent(_componentType);
+		helpMessage = "Flip switches using !{0} flip ";
+	}
+
+	private int? TryParse(string input)
+	{
+		int i;
+		return int.TryParse(input, out i) ? (int?) i : null;
+	}
+	
+	protected override IEnumerator RespondToCommandInternal(string inputCommand)
+	{
+		var commands = inputCommand.ToLowerInvariant().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+		if (commands.Length > 1 && (commands[0].Equals("flip") || commands[0].Equals("switch") || commands[0].Equals("press")))
+		{
+			var switches = commands.Where((_, i) => i > 0).Select(n => TryParse(n));
+			
+			if (switches.All(n => n != null && n > 0 && n < 6))
+			{
+				foreach (int? switchIndex in switches)
+				{
+					_OnToggleMethod.Invoke(_component, new object[] { switchIndex - 1 });
+					yield return new WaitForSeconds(0.1f);
+				}
+			}
+		}
+
+		yield return null;
+	}
+
+	static SwitchesComponentSolver()
+	{
+		_componentType = ReflectionHelper.FindType("SwitchModule");
+		_OnToggleMethod = _componentType.GetMethod("OnToggle", BindingFlags.NonPublic | BindingFlags.Instance);
+	}
+
+	private static Type _componentType = null;
+	private static MethodInfo _OnToggleMethod = null;
+
+	private object _component = null;
+}
