cbc27e6ee09a47179c205f5b96f5daad0c1a5cbb
samfun123 <samdinner@hotmail.com>
2018-02-07 11:13:38 -0500

    First version of KTANE Tweaks

---
+++ Tweaks/TweaksAssembly/Tweaks.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(KMService))]
+public class Tweaks : MonoBehaviour
+{
+	void Awake()
+	{
+		FreeplayDevice.MAX_SECONDS_TO_SOLVE = float.MaxValue;
+		FreeplayDevice.MIN_MODULE_COUNT = 1;
+
+		UnityEngine.SceneManagement.SceneManager.sceneLoaded += delegate (Scene scene, LoadSceneMode _)
+		{
+			if (scene.name == "gameplayLoadingScene") {
+				var loadmanager = FindObjectOfType<GameplayLoadingManager>();
+				loadmanager.MinTotalLoadTime = 0;
+				loadmanager.FadeInTime = 0.5f;
+				loadmanager.FadeOutTime = 0.5f;
+			}
+		};
+	}
+}
