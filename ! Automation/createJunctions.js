a5e071978eddd0b3a2f0497fecd1d47e66251bef
samfun123 <samdinner@hotmail.com>
2018-02-06 20:09:01 -0500

    Add automatic junction maker script

---
+++ ! Automation/createJunctions.js
@@ -0,0 +1,49 @@
+const fs = require("fs-extra");
+const { promisify } = require("util");
+const { join, resolve } = require("path");
+
+const readdir = promisify(fs.readdir);
+const stat = promisify(fs.stat);
+const lstat = promisify(fs.lstat);
+const symlink = promisify(fs.symlink);
+const access = promisify(fs.access);
+
+function catchify(promise) {
+	return promise
+		.then(resp => [null, resp])
+		.catch(err => [err, null]);
+} 
+
+const junctions = [
+	"Assets\\Editor\\Scripts",
+	"Assets\\Editor\\Steamworks.NET",
+	"Assets\\Plugins",
+	"Assets\\KM_Assets",
+	"Assets\\TestHarness",
+	"ProjectSettings",
+];
+
+process.chdir("..");
+
+(async function() {
+	let files = await readdir(".", undefined);
+	for (let file of files) {
+		if (file != "! Template Project" && (file.startsWith("! ") || file.startsWith("."))) continue;
+
+		let stats = await stat(file);
+		if (!stats.isDirectory()) continue;
+
+		for (let junc of junctions) {
+			let folder = join(file, junc);
+
+			let [err, stats] = await catchify(lstat(folder));
+			if (err == null) {
+				if (stats.isSymbolicLink()) continue;
+				else await fs.remove(folder);
+			}
+
+			await symlink(resolve(join("! Source Project", junc)), folder, "junction");
+			console.log(`Created junction for "${folder}"`);
+		}
+	}
+})();
