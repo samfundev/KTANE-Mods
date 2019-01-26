6dfb1c40278d10c321fbb6f690cd873f88603476
Timwi <github@timwi.de>
2019-01-26 16:43:15 +0100

    Add logging for Emoji Math and Switches

---
+++ Tweaks/TweaksAssembly/Modules/EmojiMathLogging.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+
+public class EmojiMathLogging : ModuleTweak
+{
+    const string typeName = "EmojiMathModule";
+
+    private static int idCounter = 1;
+    private readonly int moduleID;
+    private readonly KMBombInfo bombInfo;
+
+    public EmojiMathLogging(BombComponent bombComponent) : base(bombComponent)
+    {
+        componentType = componentType ?? (componentType = ReflectionHelper.FindType(typeName));
+        component = bombComponent.GetComponent(componentType);
+        moduleID = idCounter++;
+        bombInfo = bombComponent.GetComponent<KMBombInfo>();
+
+        mPuzzle = componentType?.GetField("Puzzle", BindingFlags.NonPublic | BindingFlags.Instance);
+        mButtons = componentType?.GetField("Buttons", BindingFlags.Public | BindingFlags.Instance);
+        mSign = componentType?.GetField("Sign", BindingFlags.NonPublic | BindingFlags.Instance);
+        mAnswer = componentType?.GetField("Answer", BindingFlags.NonPublic | BindingFlags.Instance);
+        mDisplayText = componentType?.GetField("DisplayText", BindingFlags.Public | BindingFlags.Instance);
+
+        if (componentType == null || component == null || mPuzzle == null || mButtons == null || mSign == null || mAnswer == null || mDisplayText == null)
+        {
+            Debug.Log($"[Emoji Math #{moduleID}] Logging failed (1): {new object[] { componentType, component, mPuzzle, mButtons, mSign, mAnswer, mDisplayText }.Select(obj => obj == null ? "<NULL>" : "(not null)").Join(", ")}.");
+            return;
+        }
+
+        bombComponent.GetComponent<KMBombModule>().OnActivate += () =>
+        {
+            var buttons = (KMSelectable[]) mButtons.GetValue(component);
+            for (int i = 0; i < buttons.Length; i++)
+                bindButtonPress(buttons[i], i);
+
+            var puzzle = mPuzzle.GetValue(component);
+            getPuzzleFieldsMethods(puzzle.GetType());
+            var op1 = (int) mOperand1.GetValue(puzzle);
+            var op2 = (int) mOperand2.GetValue(puzzle);
+            var op = (string) mGetOperationString.Invoke(null, new object[] { mOperator.GetValue(puzzle) });
+            Debug.Log($"[Emoji Math #{moduleID}] Puzzle on module: “{((TextMesh) mDisplayText.GetValue(component)).text}”");
+            Debug.Log($"[Emoji Math #{moduleID}] Decoded puzzle: “{op1} {op} {op2}”");
+            Debug.Log($"[Emoji Math #{moduleID}] Expected answer: “{(op == "+" ? op1 + op2 : op1 - op2)}”");
+        };
+    }
+
+    private void bindButtonPress(KMSelectable sel, int i)
+    {
+        var prev = sel.OnInteract;
+        sel.OnInteract = delegate
+        {
+            var prevAnswer = (string) mAnswer.GetValue(component);
+            var ret = prev();
+            switch (i)
+            {
+                case 10:    // Minus button
+                    Debug.Log($"[Emoji Math #{moduleID}] You pressed Minus. Sign is now {(mSign.GetValue(component).Equals(1) ? "positive." : "negative. Remember that this sticks even across strikes!")}");
+                    break;
+
+                case 11:    // Enter button
+                    Debug.Log($"[Emoji Math #{moduleID}] You submitted “{(mSign.GetValue(component).Equals(1) ? "" : "-")}{prevAnswer}”. This is {(mCheckAnswer.Invoke(mPuzzle.GetValue(component), new object[] { prevAnswer, mSign.GetValue(component) }).Equals(true) ? "correct. Module solved." : "wrong. Strike!")}");
+                    break;
+
+                default:    // Digit
+                    Debug.Log($"[Emoji Math #{moduleID}] You pressed {i}. Input is now “{(mSign.GetValue(component).Equals(1) ? "" : "-")}{mAnswer.GetValue(component)}”.");
+                    break;
+            }
+            return ret;
+        };
+    }
+
+    private void getPuzzleFieldsMethods(Type puzzleType)
+    {
+        mCheckAnswer = puzzleType.GetMethod("CheckAnswer", BindingFlags.Public | BindingFlags.Instance);
+        mGetOperationString = puzzleType.GetMethod("GetOperationString", BindingFlags.Public | BindingFlags.Static);
+        mOperand1 = puzzleType.GetField("Operand1", BindingFlags.Public | BindingFlags.Instance);
+        mOperand2 = puzzleType.GetField("Operand2", BindingFlags.Public | BindingFlags.Instance);
+        mOperator = puzzleType.GetField("Operator", BindingFlags.Public | BindingFlags.Instance);
+
+        if (mCheckAnswer == null || mGetOperationString == null || mOperand1 == null || mOperand2 == null || mOperator == null)
+            Debug.Log($"[Emoji Math #{moduleID}] Logging failed (2): {new object[] { mCheckAnswer, mGetOperationString, mOperand1, mOperand2, mOperator }.Select(obj => obj == null ? "<NULL>" : "(not null)").Join(", ")}.");
+    }
+
+    static Type componentType;
+
+    // Fields on the component type
+    static FieldInfo mPuzzle;
+    static FieldInfo mButtons;
+    static FieldInfo mSign;
+    static FieldInfo mAnswer;
+    static FieldInfo mDisplayText;
+
+    // Fields/methods on the Puzzle type
+    static MethodInfo mCheckAnswer;
+    static MethodInfo mGetOperationString;
+    static FieldInfo mOperand1;
+    static FieldInfo mOperand2;
+    static FieldInfo mOperator;
+}
--- Tweaks/TweaksAssembly/Modules/ProbingLogging.cs
+++ Tweaks/TweaksAssembly/Modules/ProbingLogging.cs
@@ -7,119 +7,119 @@ using UnityEngine;
 
 public class ProbingLogging : ModuleTweak
 {
-	static readonly string typeName = "ProbingModule";
+    const string typeName = "ProbingModule";
 
-	[Flags]
-	private enum WireValues
-	{
-		NONE = 0x0,
-		A = 0x1,
-		B = 0x2,
-		C = 0x4,
-		D = 0x8
-	}
+    [Flags]
+    private enum WireValues
+    {
+        NONE = 0x0,
+        A = 0x1,
+        B = 0x2,
+        C = 0x4,
+        D = 0x8
+    }
 
-	Dictionary<WireValues, string> wireHz = new Dictionary<WireValues, string>()
-	{
-		{ WireValues.A, "10" },
-		{ WireValues.B, "22" },
-		{ WireValues.C, "50" },
-		{ WireValues.D, "60" }
-	};
+    Dictionary<WireValues, string> wireHz = new Dictionary<WireValues, string>()
+    {
+        { WireValues.A, "10" },
+        { WireValues.B, "22" },
+        { WireValues.C, "50" },
+        { WireValues.D, "60" }
+    };
 
-	// Depending on the value in the target wire, we can deduce the rule used for both clips.
-	Dictionary<WireValues, string> redClipRules = new Dictionary<WireValues, string>()
-	{
+    // Depending on the value in the target wire, we can deduce the rule used for both clips.
+    readonly Dictionary<WireValues, string> redClipRules = new Dictionary<WireValues, string>()
+    {
 		{ WireValues.A | WireValues.B | WireValues.D, "the red and white wire contains 50 Hz" },
-		{ WireValues.B | WireValues.C | WireValues.D, "the red and yellow wire doesn't contain 10 Hz" },
-		{ WireValues.A | WireValues.B | WireValues.C, "the other two conditions didn't apply" }
-	};
+        { WireValues.B | WireValues.C | WireValues.D, "the red and yellow wire doesn't contain 10 Hz" },
+        { WireValues.A | WireValues.B | WireValues.C, "the other two conditions didn't apply" }
+    };
 
-	Dictionary<WireValues, string> blueClipRules = new Dictionary<WireValues, string>()
-	{
-		{ WireValues.A | WireValues.C | WireValues.D, "the red and yellow wire contains 10 Hz" },
-		{ WireValues.A | WireValues.B | WireValues.C, "the other condition didn't apply" }
-	};
+    readonly Dictionary<WireValues, string> blueClipRules = new Dictionary<WireValues, string>()
+    {
+        { WireValues.A | WireValues.C | WireValues.D, "the red and yellow wire contains 10 Hz" },
+        { WireValues.A | WireValues.B | WireValues.C, "the other condition didn't apply" }
+    };
 
-	private static int idCounter = 1;
-	private readonly int moduleID;
-	private readonly KMBombInfo bombInfo;
+    private static int idCounter = 1;
+    private readonly int moduleID;
+    private readonly KMBombInfo bombInfo;
 
-	public ProbingLogging(BombComponent bombComponent) : base(bombComponent)
-	{
-		componentType = componentType ?? (componentType = ReflectionHelper.FindType(typeName));
-		mWiresField = mWiresField ?? (mWiresField = componentType.GetField("mWires", NonPublicInstance));
-		mTargetWireAField = mTargetWireAField ?? (mTargetWireAField = componentType.GetField("mTargetWireA", NonPublicInstance));
-		mTargetWireBField = mTargetWireBField ?? (mTargetWireBField = componentType.GetField("mTargetWireB", NonPublicInstance));
+    public ProbingLogging(BombComponent bombComponent) : base(bombComponent)
+    {
+        componentType = componentType ?? (componentType = ReflectionHelper.FindType(typeName));
+        mWiresField = mWiresField ?? (mWiresField = componentType.GetField("mWires", NonPublicInstance));
+        mTargetWireAField = mTargetWireAField ?? (mTargetWireAField = componentType.GetField("mTargetWireA", NonPublicInstance));
+        mTargetWireBField = mTargetWireBField ?? (mTargetWireBField = componentType.GetField("mTargetWireB", NonPublicInstance));
 
-		component = bombComponent.GetComponent(componentType);
-		moduleID = idCounter++;
-		bombInfo = bombComponent.GetComponent<KMBombInfo>();
-		mNumStrikes = bombInfo.GetStrikes();
-		bombComponent.GetComponent<KMBombModule>().OnActivate += () => bActive = true;
-		bombComponent.GetComponent<KMBombModule>().OnPass += () =>
-		{
-			Debug.Log($"[Probing #{moduleID}] Module solved.");
-			return true;
-		};
+        component = bombComponent.GetComponent(componentType);
+        moduleID = idCounter++;
+        bombInfo = bombComponent.GetComponent<KMBombInfo>();
+        mNumStrikes = bombInfo.GetStrikes();
+        bombComponent.GetComponent<KMBombModule>().OnActivate += () => bActive = true;
+        bombComponent.GetComponent<KMBombModule>().OnPass += () =>
+        {
+            Debug.Log($"[Probing #{moduleID}] Module solved.");
+            return true;
+        };
 
-		mWires = ((Array) mWiresField.GetValue(component)).Cast<WireValues>();
-		LogWires();
-		bombComponent.StartCoroutine(CheckForStrikeChanges());
-	}
+        mWires = ((Array) mWiresField.GetValue(component)).Cast<WireValues>();
+        LogWires();
+        bombComponent.StartCoroutine(CheckForStrikeChanges());
+    }
 
-	private int mNumStrikes;
-	private bool bActive;
-	private IEnumerable<WireValues> mWires;
-	IEnumerator CheckForStrikeChanges()
-	{
-		while (true)
-		{
-			if (bActive && mNumStrikes != bombInfo.GetStrikes())
-			{
-				mNumStrikes = bombInfo.GetStrikes();
+    private int mNumStrikes;
+    private bool bActive;
+    private IEnumerable<WireValues> mWires;
+    IEnumerator CheckForStrikeChanges()
+    {
+        while (true)
+        {
+            if (bActive && mNumStrikes != bombInfo.GetStrikes())
+            {
+                mNumStrikes = bombInfo.GetStrikes();
 
-				var mWiresNew = ((Array) mWiresField.GetValue(component)).Cast<WireValues>();
-				var valuesChanged = !mWires.SequenceEqual(mWiresNew);
+                var mWiresNew = ((Array) mWiresField.GetValue(component)).Cast<WireValues>();
+                var valuesChanged = !mWires.SequenceEqual(mWiresNew);
 
-				mWires = mWiresNew;
-				if (valuesChanged)
-				{
-					Debug.Log($"[Probing #{moduleID}] The number of strikes changed and so did the wire values.");
-					LogWires();
-				}
-				else
-					Debug.Log($"[Probing #{moduleID}] The number of strikes changed but the wire values stayed the same.");
-			}
-			yield return null;
-		}
-	}
+                mWires = mWiresNew;
+                if (valuesChanged)
+                {
+                    Debug.Log($"[Probing #{moduleID}] The number of strikes changed and so did the wire values.");
+                    LogWires();
+                }
+                else
+                    Debug.Log($"[Probing #{moduleID}] The number of strikes changed but the wire values stayed the same.");
+            }
+            yield return null;
+        }
+    }
 
-	string FormatWireValue(WireValues wireValue) => wireHz
-		.Where(pair => (wireValue & pair.Key) != 0)
-		.Select(pair => pair.Value)
-		.Join("+");
+    string FormatWireValue(WireValues wireValue) => wireHz
+        .Where(pair => (wireValue & pair.Key) != 0)
+        .Select(pair => pair.Value)
+        .Join("+");
 
-	void LogWires()
-	{
-		var wireValues = mWires.Select(FormatWireValue);
+    void LogWires()
+    {
+        var wireValues = mWires.Select(FormatWireValue);
 
-		var missingWireValues = mWires.Select(wireValue => wireHz.First(pair => (wireValue & pair.Key) == 0).Value);
+        var missingWireValues = mWires.Select(wireValue => wireHz.First(pair => (wireValue & pair.Key) == 0).Value);
 
-		Debug.Log($"[Probing #{moduleID}] Wire values:\n{wireValues.Take(3).Join(" | ")}\n{wireValues.Skip(3).Join(" | ")}");
-		Debug.Log($"[Probing #{moduleID}] Missing wire values:\n{missingWireValues.Take(3).Join(" | ")}\n{missingWireValues.Skip(3).Join(" | ")}");
+        Debug.Log($"[Probing #{moduleID}] Wire values:\n{wireValues.Take(3).Join(" | ")}\n{wireValues.Skip(3).Join(" | ")}");
+        Debug.Log($"[Probing #{moduleID}] Missing wire values:\n{missingWireValues.Take(3).Join(" | ")}\n{missingWireValues.Skip(3).Join(" | ")}");
 
-		WireValues redTargetValues = (WireValues) mTargetWireAField.GetValue(component);
-		WireValues blueTargetValues = (WireValues) mTargetWireBField.GetValue(component);
+        WireValues redTargetValues = (WireValues) mTargetWireAField.GetValue(component);
+        WireValues blueTargetValues = (WireValues) mTargetWireBField.GetValue(component);
 
-		Debug.Log($"[Probing #{moduleID}] The red clip should be connected to a wire containing {FormatWireValue(redTargetValues)} because {redClipRules[redTargetValues]}.");
-		Debug.Log($"[Probing #{moduleID}] The blue clip should be connected to a wire containing {FormatWireValue(blueTargetValues)} because {blueClipRules[blueTargetValues]}.");
-	}
+        Debug.Log($"[Probing #{moduleID}] The red clip should be connected to a wire containing {FormatWireValue(redTargetValues)} because {redClipRules[redTargetValues]}.");
+        Debug.Log($"[Probing #{moduleID}] The blue clip should be connected to a wire containing {FormatWireValue(blueTargetValues)} because {blueClipRules[blueTargetValues]}.");
+    }
 
-	static Type componentType;
-	static FieldInfo mWiresField;
-	static FieldInfo mTargetWireAField;
-	static FieldInfo mTargetWireBField;
+    static Type componentType;
+    static FieldInfo mWiresField;
+    static FieldInfo mTargetWireAField;
+    static FieldInfo mTargetWireBField;
 
-	const BindingFlags NonPublicInstance = BindingFlags.NonPublic | BindingFlags.Instance;
+    const BindingFlags NonPublicInstance = BindingFlags.NonPublic | BindingFlags.Instance;
 }
