265312e74b3270de39119246a41839069c18ca6f
samfun123 <samdinner@hotmail.com>
2019-07-01 13:07:03 -0400

    Tweaks: Use reflection helper methods in Sea Shells logging

--- Tweaks/TweaksAssembly/Modules/SeaShellsLogging.cs
+++ Tweaks/TweaksAssembly/Modules/SeaShellsLogging.cs
@@ -14,11 +14,6 @@ public class SeaShellsLogging : ModuleLogging
 	public SeaShellsLogging(BombComponent bombComponent) : base(bombComponent, "Sea Shells")
 	{
 		SeaShellsModule = bombComponent.gameObject.GetComponent(typeName);
-		mButtonsField = mButtonsField ?? (mButtonsField = SeaShellsModule.GetType().GetField("buttons", PublicInstance));
-		mDisplayField = mDisplayField ?? (mDisplayField = SeaShellsModule.GetType().GetField("Display", PublicInstance));
-		mStepField = mStepField ?? (mStepField = SeaShellsModule.GetType().GetField("step", NonPublicInstance));
-		mStageField = mStageField ?? (mStageField = SeaShellsModule.GetType().GetField("stage", NonPublicInstance));
-
 		bombComponent.GetComponent<KMBombModule>().OnPass += () =>
 		{
 			Solved = true;
@@ -27,25 +22,18 @@ public class SeaShellsLogging : ModuleLogging
 
 		bombComponent.GetComponent<KMBombModule>().OnActivate += () =>
 		{
-			var checker = bombComponent.StartCoroutine(RunLogger(
-				fldLength: SeaShellsModule.GetType().GetField("length", NonPublicInstance),
-				fldTable: SeaShellsModule.GetType().GetField("table", NonPublicInstance),
-				fldKey: SeaShellsModule.GetType().GetField("key", NonPublicInstance),
-				fldSwap: SeaShellsModule.GetType().GetField("swap", NonPublicInstance),
-				fldKeynum: SeaShellsModule.GetType().GetField("keynum", NonPublicInstance),
-				fldRow: SeaShellsModule.GetType().GetField("row", NonPublicInstance),
-				fldCol: SeaShellsModule.GetType().GetField("col", NonPublicInstance)));
+			var checker = bombComponent.StartCoroutine(RunLogger());
 
-			var buttons = (KMSelectable[]) mButtonsField.GetValue(SeaShellsModule);
+			var buttons = SeaShellsModule.GetValue<KMSelectable[]>("buttons");
 			var oldHandlers = buttons.Select(button => button.OnInteract).ToArray();
 			for (int i = 0; i < buttons.Length; i++)
 			{
 				var j = i;
 				buttons[i].OnInteract = delegate
 				{
-					var prevStep = (int) mStepField.GetValue(SeaShellsModule);
+					var prevStep = SeaShellsModule.GetValue<int>("step");
 					var ret = oldHandlers[j]();
-					if (Solved || (int) mStepField.GetValue(SeaShellsModule) > prevStep)
+					if (Solved || SeaShellsModule.GetValue<int>("step") > prevStep)
 					{
 						Log($"{buttons[j].GetComponentInChildren<TextMesh>().text}: correct");
 						if (Solved)
@@ -64,9 +52,9 @@ public class SeaShellsLogging : ModuleLogging
 		};
 	}
 
-	private IEnumerator RunLogger(FieldInfo fldLength, FieldInfo fldTable, FieldInfo fldKey, FieldInfo fldSwap, FieldInfo fldKeynum, FieldInfo fldRow, FieldInfo fldCol)
+	private IEnumerator RunLogger()
 	{
-		var buttons = (KMSelectable[]) mButtonsField.GetValue(SeaShellsModule);
+		var buttons = SeaShellsModule.GetValue<KMSelectable[]>("buttons");
 		while (true)
 		{
 			while (buttons.Any(b => b.GetComponentInChildren<TextMesh>().text == " "))
@@ -76,20 +64,18 @@ public class SeaShellsLogging : ModuleLogging
 					yield break;
 			}
 
-			// this.OnPress(this.swap[j] == this.key[this.keynum, this.table[this.row, this.col, this.step]]);
-
-			Log($"Stage: {(int) mStageField.GetValue(SeaShellsModule) + 1}");
-			Log($"Phrase: {((TextMesh) mDisplayField.GetValue(SeaShellsModule)).text.Replace("\n", " ")}");
+			Log($"Stage: {SeaShellsModule.GetValue<int>("stage") + 1}");
+			Log($"Phrase: {SeaShellsModule.GetValue<TextMesh>("Display").text.Replace("\n", " ")}");
 			Log($"Buttons: {buttons.Select(b => b.GetComponentInChildren<TextMesh>().text).Join(", ")}");
 
-			var row = (int) fldRow.GetValue(SeaShellsModule);
-			var col = (int) fldCol.GetValue(SeaShellsModule);
-			var key = (int[,]) fldKey.GetValue(SeaShellsModule);
-			var keynum = (int) fldKeynum.GetValue(SeaShellsModule);
-			var table = (int[,,]) fldTable.GetValue(SeaShellsModule);
-			var swap = (int[]) fldSwap.GetValue(SeaShellsModule);
+			var row = SeaShellsModule.GetValue<int>("row");
+			var col = SeaShellsModule.GetValue<int>("col");
+			var key = SeaShellsModule.GetValue<int[,]>("key");
+			var keynum = SeaShellsModule.GetValue<int>("keynum");
+			var table = SeaShellsModule.GetValue<int[,,]>("table");
+			var swap = SeaShellsModule.GetValue<int[]>("swap");
 
-			Log($@"Expected solution: {Enumerable.Range(0, ((int[,]) fldLength.GetValue(SeaShellsModule))[row, col])
+			Log($@"Expected solution: {Enumerable.Range(0, SeaShellsModule.GetValue<int[,]>("length")[row, col])
 				.Select(i => buttons[Array.IndexOf(swap, key[keynum, table[row, col, i]])].GetComponentInChildren<TextMesh>().text)
 				.Join(", ")}");
 
@@ -101,12 +87,4 @@ public class SeaShellsLogging : ModuleLogging
 			}
 		}
 	}
-
-	static FieldInfo mButtonsField;
-	static FieldInfo mDisplayField;
-	static FieldInfo mStepField;
-	static FieldInfo mStageField;
-
-	const BindingFlags NonPublicInstance = BindingFlags.NonPublic | BindingFlags.Instance;
-	const BindingFlags PublicInstance = BindingFlags.Public | BindingFlags.Instance;
 }
