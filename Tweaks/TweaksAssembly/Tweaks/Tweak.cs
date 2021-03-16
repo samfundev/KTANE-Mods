ac7ce61a3407708bf2a98c3ec1c44fef16b74407
samfundev <samdinner@hotmail.com>
2021-03-15 20:14:53 -0400

    Tweaks: Allow a tweak to enabled and setup themselves

--- Tweaks/TweaksAssembly/Tweaks/Tweak.cs
+++ Tweaks/TweaksAssembly/Tweaks/Tweak.cs
@@ -4,7 +4,17 @@ public abstract class Tweak
 {
 	protected Tweak()
 	{
+		if (!Enabled)
+			return;
+
+		Setup();
 		Tweaks.OnStateChanged += OnStateChange;
+	}
+
+	public virtual bool Enabled => true;
+
+	public virtual void Setup()
+	{
 	}
 
 	public virtual void OnStateChange(State previousState, State state)
