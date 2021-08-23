abe506ce0e4a8aaa0faec6b80cb12d95e8f7f986
Brendan King <brendan@brendanking.com>
2021-08-22 22:42:12 -0400

    Implement suggested changes and add Grocery Store tweak

---
+++ Tweaks/TweaksAssembly/Modules/GroceryStoreTweak.cs
@@ -0,0 +1,12 @@
+﻿class GroceryStoreTweak : ModuleTweak
+{
+	public GroceryStoreTweak(BombComponent bombComponent) : base(bombComponent, "GroceryStoreBehav")
+	{
+		// Remove the ability to press the buttons after the module is solved
+		bombComponent.OnPass += (_) => {
+			for (int i = 0; i < 2; i++)
+				bombComponent.GetComponent<KMSelectable>().Children[i].OnInteract = null;
+			return false;
+		};
+	}
+}
