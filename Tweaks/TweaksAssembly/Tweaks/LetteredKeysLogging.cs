6d13d33e6094695ba489070edad8129ebe5a1980
Nya Bentley <nyabentley@gmail.com>
2022-09-06 23:14:16 -0400

    Inital Lettered keys logging

---
+++ Tweaks/TweaksAssembly/Tweaks/LetteredKeysLogging.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+
+public class LetteredKeysLogging : ModuleLogging
+{
+    public LetteredKeysLogging(BombComponent bombComponent) : base(bombComponent, "LetterKeys", "Letter Keys")
+    {
+
+		bombComponent.GetComponent<KMBombModule>().OnActivate += () =>
+        {
+            Log($"Lettered Keys logging has been initialized");
+        };
+
+		bombComponent.GetComponent<KMBombModule>().OnPass += () =>
+        {
+			Log($"You solved the lettered keys module");
+			return false;
+		};
+
+		bombComponent.GetComponent<KMBombModule>().OnStrike += () =>
+        {
+			Log($"You struck on the lettered keys module");
+			return false;
+        };
+	}
+
+
+}
