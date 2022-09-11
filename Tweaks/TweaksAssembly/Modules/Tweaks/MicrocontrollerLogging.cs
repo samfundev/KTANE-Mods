4113144ac2018ea98cd9e39cbc7c424e01796d2a
Nya Bentley <nyabentley@gmail.com>
2022-09-11 08:53:38 -0400

    Initial commiit

---
+++ Tweaks/TweaksAssembly/Modules/Tweaks/MicrocontrollerLogging.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class MicrocontrollerLogging : ModuleLogging
+{
+	public MicrocontrollerLogging(BombComponent bombComponent) : base(bombComponent, "Microcontroller", "Microcontroller")
+	{
+
+		bombComponent.GetComponent<KMBombModule>().OnActivate += () =>
+		{
+			Log("Microcontroller log initialization");
+		};
+
+		bombComponent.GetComponent<KMBombModule>().OnPass += () =>
+		{
+			Log("Module Solved");
+			return false;
+		};
+
+		bombComponent.GetComponent<KMBombModule>().OnStrike += () =>
+		{
+			Log("Module Stiked");
+			return false;
+		};
+	}
+}
