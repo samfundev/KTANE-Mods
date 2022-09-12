381af05f5d349f513370b25359c510b01076f8eb
Nya Bentley <nyabentley@gmail.com>
2022-09-12 00:34:16 -0400

    Tells what each pin is and what color it's supposed to be

--- Tweaks/TweaksAssembly/Modules/Tweaks/MicrocontrollerLogging.cs
+++ Tweaks/TweaksAssembly/Modules/Tweaks/MicrocontrollerLogging.cs
@@ -4,24 +4,216 @@ using UnityEngine;
 
 public class MicrocontrollerLogging : ModuleLogging
 {
-	public MicrocontrollerLogging(BombComponent bombComponent) : base(bombComponent, "Microcontroller", "Microcontroller")
+	public MicrocontrollerLogging(BombComponent bombComponent) : base(bombComponent, "Micro", "Microcontroller")
 	{
 
 		bombComponent.GetComponent<KMBombModule>().OnActivate += () =>
 		{
-			Log("Microcontroller log initialization");
-		};
+			int[] colorMap = component.GetValue<int[]>("colorMap");
+			int[] solutionRaw = component.GetValue<int[]>("solutionRaw");
+			int colorRow = GetRow(colorMap);
+;
 
-		bombComponent.GetComponent<KMBombModule>().OnPass += () =>
-		{
-			Log("Module Solved");
-			return false;
-		};
+			Dictionary<string, string> pinColor = new Dictionary<string, string>();
 
-		bombComponent.GetComponent<KMBombModule>().OnStrike += () =>
-		{
-			Log("Module Stiked");
-			return false;
+			Log($"Controller Type: {component.GetValue<TextMesh>("MicType").text}");
+			Log($"Controller Serial: {component.GetValue<TextMesh>("MicSerial").text.Substring(4)}");
+			//Log($"Dot Pos: {GetDotPos(component.GetValue<int>("dotPos"))}");
+			Log($"Using Row: {colorRow}");
+
+
+			List<string> ledNames = new List<string>();
+
+			for(int i = 0; i < solutionRaw.Length; i++)
+			{ 
+				string pinName = GetPinName(solutionRaw[i]);
+
+				if (!pinColor.ContainsKey(pinName))
+				{
+					pinColor.Add(pinName, GetColor(colorRow, pinName));
+				}
+
+				Log($"{i + 1}. {pinName} = {pinColor[pinName]}");
+			}
 		};
+	}
+
+	private string GetPinName(int i)
+	{
+		switch (i)
+		{
+			case 0:
+				return "GND";
+
+			case 1:
+				return "VCC";
+
+			case 2:
+				return "AIN";
+
+			case 3:
+				return "DIN";
+
+			case 4:
+				return "PWN";
+
+			default:
+				return "RST";
+		}
+	}
+
+	private int GetRow(int[] colorMap)
+	{
+		if (colorMap.SequenceEqual(new int[] { 1, 3, 4, 6, 5, 2 }))
+		{
+			return 1;
+		}
+
+		if (colorMap.SequenceEqual(new int[] { 1, 3, 2, 4, 6, 5 }))
+		{
+			return 2;
+		}
+
+		if (colorMap.SequenceEqual(new int[] { 1, 2, 4, 6, 5, 3 }))
+		{
+			return 3;
+		}
+
+		if (colorMap.SequenceEqual(new int[] { 1, 2, 5, 3, 6, 4 }))
+		{
+			return 4;
+		}
+
+		return 5;
+	}
+
+	private string GetColor(int colorRow, string pinName)
+	{
+		if (colorRow == 1)
+		{
+			switch (pinName)
+			{
+				case "VCC":
+					return "Yellow";
+
+				case "AIN":
+					return "Magenta";
+
+				case "DIN":
+					return "Green";
+
+				case "PWN":
+					return "Blue";
+
+				case "RST":
+					return "Red";
+
+				default:
+					return "White";
+
+			}
+		}
+
+		if (colorRow == 2)
+		{
+			switch (pinName)
+			{
+				case "VCC":
+					return "Yellow";
+
+				case "AIN":
+					return "Red";
+
+				case "DIN":
+					return "Magenta";
+
+				case "PWN":
+					return "Green";
+
+				case "RST":
+					return "Blue";
+
+				default:
+					return "White";
+
+			}
+		}
+
+		if (colorRow == 3)
+		{
+			switch (pinName)
+			{
+				case "VCC":
+					return "Red";
+
+				case "AIN":
+					return "Magenta";
+
+				case "DIN":
+					return "Green";
+
+				case "PWN":
+					return "Blue";
+
+				case "RST":
+					return "Yellow";
+
+				default:
+					return "White";
+
+			}
+		}
+
+		if (colorRow == 4)
+		{
+			switch (pinName)
+			{
+				case "VCC":
+					return "Red";
+
+				case "AIN":
+					return "Blue";
+
+				case "DIN":
+					return "Yellow";
+
+				case "PWN":
+					return "Green";
+
+				case "RST":
+					return "Magenta";
+
+				default:
+					return "White";
+
+			}
+		}
+
+		else
+		{
+			switch (pinName)
+			{
+				case "VCC":
+					return "Green";
+
+				case "AIN":
+					return "Red";
+
+				case "DIN":
+					return "Yellow";
+
+				case "PWN":
+					return "Blue";
+
+				case "RST":
+					return "Magenta";
+
+				default:
+					return "White";
+
+			}
+		}
+
+
 	}
 }
