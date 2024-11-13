1d8cd4e30e36e5bb502d406888b5c68a1ab1322a
Hawker <62062830+BlckHawker@users.noreply.github.com>
2024-11-13 16:03:46 -0500

    Fix TweaksAssembly

--- Tweaks/TweaksAssembly/Modules/Tweaks/AnagramsLogging.cs
+++ Tweaks/TweaksAssembly/Modules/Tweaks/AnagramsLogging.cs
@@ -12,11 +12,6 @@ namespace TweaksAssembly.Modules.Tweaks
 		{
 			bombComponent.GetComponent<KMBombModule>().OnActivate += () =>
 			{
-
-				//Displayed word: {1}
-				//Possible answers: {1}, {2}
-				//Submitted {1}. (Solving module... | Strike!)
-
 				string displayedWord = component.GetValue<TextMesh>("PuzzleDisplay").text;
 				 IList<IList<string>> words = component.GetValue<IList<IList<string>>>("Words");
 				List<string> possileAnswers = words.First(row => row.Contains(displayedWord)).Where(word => word != displayedWord).ToList();
